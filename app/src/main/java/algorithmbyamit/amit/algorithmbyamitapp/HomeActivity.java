ec3a03c64985b309dfcf56a3ff4e6ac9804cf352
maityamit <maityamit308@gmail.com>
2022-11-11 01:58:31 +0530

    add

---
+++ app/src/main/java/algorithmbyamit/amit/algorithmbyamitapp/HomeActivity.java
@@ -0,0 +1,28 @@
+package algorithmbyamit.amit.algorithmbyamitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        button = findViewById(R.id.home_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
